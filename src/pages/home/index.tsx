5b73b60232fc175aa01f8fe5d878538b208748d9
Ramsolz6666 <houzhicongming@qq.com>
2021-08-22 19:50:41 +0800

    fetch:完成首页指南

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,24 @@
 import React, { FC } from 'react'
-import { Card} from 'antd'
+import {Card,message} from 'antd'
 
 
 
 const Home: FC = () => {
-
-
+  const ok=()=>{
+    //@ts-ignore 
+    document.getElementById("text").select();
+    document.execCommand('copy');
+    message.info("复制成功")
+  }
   return (
     <div className="home" style={{ height: '100vh', padding: 20 }}>
           <Card title="复制/粘贴BI探针" >
-            <p><a href="javascript:void(0)">复制下方代码</a>并粘贴至页面HTML的
-            <code style={{color:"blue"}}>{'<'}body{'>'}</code>中。</p>
+            <p>
+              <a //@ts-ignore 
+              onClick={ok}>复制下方代码</a>
+              并粘贴至页面HTML的<code style={{color:"blue"}}>{'<'}body{'>'}</code>中。</p>
             <p>注意：需要将代码粘贴在<code style={{color:"blue"}}>{'<'}body{'>'}</code>内容的第一行。</p>
-            <textarea style={{padding: 20,width:"100%"} }>
+            <textarea id='text' style={{padding: 20,width:"100%"}}>
               &lt;script src="https://cdn.vansin.top/jssdk-0.1.2.min.js"&gt;&lt;/script&gt;
               &lt;script new Monitor&#40;&#123;baseUrl:"https://qcgtsp.app.cloudendpoint.cn/api",
               testMode: true,consoleError: true,app_id: "byte-monitor-docs",&#125;&#41;&lt;/script&gt;
